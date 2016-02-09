888864b213d09ffc3114f433a61794ad85079a69
Michael Wang <michaelzwang92@gmail.com>
2016-02-09 15:06:21 -0800

    Add tests to user stories

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,16 @@
+// Person 1:
+
+//sum
+//As a user, I want to take a list of numbers and add each number to the previous number.
+//In the end, I want to have the sum of the entire list of numbers.
+
+//mean
+//As a user, I want to take a list of numbers and find the sum.
+//I then want to divide the sum by the total amount of numbers.
+//The result is the mean.
+
+//median
+//As a user, I want to take a list of numbers, put them in numerical order, and determine how many numbers there are.
+//If there are an odd number of numbers, I want to find the number in the middle of the list.
+//If there are an even number of numbers, I want to find the mean of the two numbers in the middle of the list.
+//The result is the median.
