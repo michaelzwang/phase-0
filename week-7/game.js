b94dfd8f5a15fdf946dab3ed8ac38c28bf6e0264
Michael Wang <michaelzwang92@gmail.com>
2016-02-09 14:40:55 -0800

    Add new characters to solution

--- week-7/game.js
+++ week-7/game.js
@@ -77,10 +77,16 @@ var moveRight = function(object) {
 
 
 // Refactored Code
+// Add goal: collect gold coin, rescue princess, defeat dragon
+// Create new variables with random position
+// When Tharin moves, check his new position
 
 var tharin = {
-	posX: 0,
-	posY: 0,
+	posX: Math.floor((Math.random()*5)+1),
+	posY: Math.floor((Math.random()*5)+1),
+	goldCount: 0,
+	weapon: "none",
+	princess: false,
 	escape: false,
 };
 
@@ -89,41 +95,104 @@ var exit = {
 	posY: Math.floor((Math.random()*5)+1)
 };
 
-var move = function(object, direction) {
+var gold = {
+	posX: Math.floor((Math.random()*5)+1),
+	posY: Math.floor((Math.random()*5)+1)
+};
+
+var sword = {
+	posX: Math.floor((Math.random()*5)+1),
+	posY: Math.floor((Math.random()*5)+1)
+};
+
+var dragon = {
+	posX: Math.floor((Math.random()*5)+1),
+	posY: Math.floor((Math.random()*5)+1)
+}
+
+var princess = {
+	posX: Math.floor((Math.random()*5)+1),
+	posY: Math.floor((Math.random()*5)+1)
+}
+
+var move = function(direction) {
 	if(direction === 'up'){
-		object.posY += 1;
+		tharin.posY += 1;
 	}
 	else if(direction === 'down'){
-		object.posY -= 1;
+		tharin.posY -= 1;
 	}
 	else if(direction === 'left'){
-		object.posX -= 1;
+		tharin.posX -= 1;
 	}
 	else if(direction === 'right'){
-		object.posX += 1;
+		tharin.posX += 1;
 	}
 	
 	console.log("Tharin is at:" + tharin.posX + "," + tharin.posY);
-	
+
+	if(tharin.posX === gold.posX && tharin.posY === gold.posY){
+		tharin.goldCount += 1;
+		console.log("Tharin found a golden coin! He put it in his bag.")
+	}
+
+	if(tharin.posX === sword.posX && tharin.posY === sword.posY){
+		tharin.weapon = "sword";
+		console.log("Tharin picked up a powerful sword!")
+	}
+
+	if(tharin.posX === princess.posX && tharin.posY === princess.posY){
+		tharin.princess = true;
+		console.log("Tharin found the beautiful princess! He must take her to safety!")
+	}
+
+	if(tharin.posX === dragon.posX && tharin.posY === dragon.posY){
+		console.log("Tharin encountered a mighty dragon in the dungeon!");
+		attack();
+	}
+
 	if(tharin.posX === exit.posX && tharin.posY === exit.posY){
-		tharin.escape = true;
-		console.log("Tharin found the exit and escaped the dungeon!")
+		console.log("Tharin found the exit!");
+		rescue();
 	}
 };
 
+var attack = function(){
+	if(tharin.weapon === "sword"){
+		console.log("Tharin defeated the mighty dragon with the powerful sword!")
+	}
+	else{
+		console.log("Tharin did not have a weapon to defend himself and was defeated by the mighty dragon. Game over!")
+	}
+};
+
+var rescue = function(){
+	if(tharin.princess === true){
+		tharin.escape = true;
+		console.log("Tharin rescued the princess and escaped the dungeon!")
+	}
+	else{
+		console.log("Tharin must rescue the princess before he can escape!")
+	}
+}
 
 
-move(tharin, 'up');
-move(tharin, 'up');
-move(tharin, 'right');
+move('up');
+move('up');
+move('right');
+move('right');
 
 console.log(exit)
-
+console.log(gold)
+console.log(sword)
+console.log(tharin)
 
 
 
 // Reflection
 //What was the most difficult part of this challenge?
+	//For me, it was difficult at first to figure out how to modify the original object.
+	//Once I figured that out, and had my basic initial solution, I was able to effectively build upon it an add more elements to the game.
 
 //What did you learn about creating objects and functions that interact with one another?
 
