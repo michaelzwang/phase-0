58625904b0eacc61eee8d4c9ca85f2d7ef3ee09c
Michael Wang <michaelzwang92@gmail.com>
2016-02-13 12:23:57 -0800

    Run tests and combine parts for group project

--- week-7/group-project/tests.js
+++ week-7/group-project/tests.js
@@ -1,4 +1,36 @@
 // Add the finished solution here when you receive it.
+
+// Sum
+
+var sum = function(array) {
+  var total = 0;
+  for (var i = 0; i < array.length; i++) {
+        total += array[i];
+  }
+  return total;
+}
+
+// Mean
+
+var mean = function(array) {
+  var total = 0;
+  for (var i = 0; i < array.length; i++) {
+        total += array[i];
+  }
+  return total/array.length;
+}
+
+// Median
+
+function median(numbers) {
+  numbers.sort( function(a,b) {return a - b;} );
+    var half = Math.floor(numbers.length/2);
+      if(numbers.length % 2)
+        return numbers[half];
+      else
+        return (numbers[half-1] + numbers[half]) / 2.0;
+}
+
 // __________________________________________
 // Tests:  Do not alter code below this line.
 
